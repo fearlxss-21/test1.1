b784bb110899018913bf467b0a4d4dec6cbbca97
Majid Almahshi <majedabmoaz@gmail.com>
2023-09-01 04:17:32 +0300

    test

--- test.js
+++ test.js
@@ -17,82 +17,82 @@ const MbtiModule = (function() {
 		ENFJ: {title: "", percentage: "2.8%", description: "", site: "https://psychomood.online/%d8%b4%d8%ae%d8%b5%d9%8a%d8%a9-enfj-%d8%a7%d9%84%d9%85%d8%af%d8%b1%d8%b3/"},
 		ENTJ: {title: "", percentage: "2.9%", description: "", site: "https://psychomood.online/%d8%b4%d8%ae%d8%b5%d9%8a%d8%a9-entj-%d8%b1%d8%a6%d9%8a%d8%b3-%d8%a7%d9%84%d8%a7%d8%b1%d9%83%d8%a7%d9%86/"}
 	};
-	let e, i, s, n, t, f, j, p;
+let e, i, s, n, t, f, j, p;
 	let type;
 	
-	function resetScores() {
+function resetScores() {
 		e = i = s = n = t = f = j = p = 0;
 		type = "";
-	}
+}
 	
-	function getScores() {
-		const inputs = document.getElementsByTagName("input");
-		Array.prototype.forEach.call(inputs, function(input) {
-			if (input.checked) {
-				switch(input.value) {
-					case 'e': e++; break;
-					case 'i': i++; break;
-					case 's': s++; break;
-					case 'n': n++; break;
-					case 't': t++; break;
-					case 'f': f++; break;
-					case 'j': j++; break;
-					case 'p': p++; break;
-				}
-			}
-		});
-	}
+function getScores() {
+	const inputs = document.getElementsByTagName("input");
+	Array.prototype.forEach.call(inputs, function(input) {
+		if (input.checked) {
+		switch(input.value) {
+			case 'e': e++; break;
+			case 'i': i++; break;
+			case 's': s++; break;
+			case 'n': n++; break;
+			case 't': t++; break;
+			case 'f': f++; break;
+			case 'j': j++; break;
+			case 'p': p++; break;
+		    }
+	          }
+	 });
+}
 	
-	function calculatePercentages() {
-		e = Math.floor(e / 10 * 100);
-		i = Math.floor(i / 10 * 100);
-		s = Math.floor(s / 20 * 100);
-		n = Math.floor(n / 20 * 100);
-		t = Math.floor(t / 20 * 100);
-		f = Math.floor(f / 20 * 100);
-		j = Math.floor(j / 20 * 100);
-		p = Math.floor(p / 20 * 100);
-	}
+function calculatePercentages() {
+	e = Math.floor(e / 10 * 100);
+	i = Math.floor(i / 10 * 100);
+	s = Math.floor(s / 20 * 100);
+	n = Math.floor(n / 20 * 100);
+	t = Math.floor(t / 20 * 100);
+	f = Math.floor(f / 20 * 100);
+	j = Math.floor(j / 20 * 100);
+	p = Math.floor(p / 20 * 100);
+}
 	
-	function createCharts() {
-		document.querySelector("#eScore").innerHTML = e;
-		document.querySelector("#iScore").innerHTML = i;
-		document.querySelector("#sScore").innerHTML = s;
-		document.querySelector("#nScore").innerHTML = n;
-		document.querySelector("#tScore").innerHTML = t;
-		document.querySelector("#fScore").innerHTML = f;
-		document.querySelector("#jScore").innerHTML = j;
-		document.querySelector("#pScore").innerHTML = p;
+function createCharts() {
+	document.querySelector("#eScore").innerHTML = e;
+	document.querySelector("#iScore").innerHTML = i;
+	document.querySelector("#sScore").innerHTML = s;
+	document.querySelector("#nScore").innerHTML = n;
+	document.querySelector("#tScore").innerHTML = t;
+	document.querySelector("#fScore").innerHTML = f;
+	document.querySelector("#jScore").innerHTML = j;
+	document.querySelector("#pScore").innerHTML = p;
 		
-		document.querySelector("#eiChart").style.marginLeft = i / 2 + "%";
-		document.querySelector("#snChart").style.marginLeft = n / 2 + "%";
-		document.querySelector("#tfChart").style.marginLeft = f / 2 + "%";
-		document.querySelector("#jpChart").style.marginLeft = p / 2 + "%";
-	}
+	document.querySelector("#eiChart").style.marginLeft = i / 2 + "%";
+	document.querySelector("#snChart").style.marginLeft = n / 2 + "%";
+	document.querySelector("#tfChart").style.marginLeft = f / 2 + "%";
+	document.querySelector("#jpChart").style.marginLeft = p / 2 + "%";
+}
 	
-	function showResults() {
-		type += (e >= i) ? "E" : "I";
-		type += (s >= n) ? "S" : "N";
-		type += (t >= f) ? "T" : "F";
-		type += (j >= p) ? "J" : "P";
-		document.querySelector("#type").innerHTML = type;
-		document.querySelector("#type-title").innerHTML = types[type].title;
-		document.querySelector("#type-percentage").innerHTML = types[type].percentage;
-		document.querySelector("#type-description").innerHTML = types[type].description;
-		document.querySelector("#type-site").href = types[type].site;
+function showResults() {
+	type += (e >= i) ? "E" : "I";
+	type += (s >= n) ? "S" : "N";
+	type += (t >= f) ? "T" : "F";
+	type += (j >= p) ? "J" : "P";
+	document.querySelector("#type").innerHTML = type;
+	document.querySelector("#type-title").innerHTML = types[type].title;
+	document.querySelector("#type-percentage").innerHTML = types[type].percentage;
+	document.querySelector("#type-description").innerHTML = types[type].description;
+	document.querySelector("#type-site").href = types[type].site;
 		
-		document.querySelector("#type-details").classList.remove("hidden");
-		document.querySelector("#scroll-down").classList.remove("hidden");
-		document.querySelector("#results").classList.remove("hidden");
-	}
+	document.querySelector("#type-details").classList.remove("hidden");
+	document.querySelector("#scroll-down").classList.remove("hidden");
+	document.querySelector("#results").classList.remove("hidden");
+}
 	
-	return {
-		processForm: function() {
-			resetScores();
-			getScores();
-			calculatePercentages();
-			createCharts();
-			showResults();
+return {
+	processForm: function() {
+		resetScores();
+		getScores();
+		calculatePercentages();
+		createCharts();
+		showResults();
 		}
 	};
 })();
